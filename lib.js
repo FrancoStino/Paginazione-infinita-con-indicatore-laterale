ac4c694cc0d7b1948f981b8fc5ec450d57860cac
Davide <info@davideladisa.it>
2025-07-08 20:52:44 +0200

    Add scroll-based pagination with indicator updates

--- lib.js
+++ lib.js
@@ -15,16 +15,27 @@ const blog = {
 window.addEventListener('scroll', () => {
   let { scrollHeight, clientHeight, scrollTop } = document.documentElement;
   let maxScroll = scrollHeight - clientHeight;
+
+  // Scroll verso il basso - carica pagina successiva
   if ((scrollTop >= maxScroll - 1) && (blog.currentPage < blog.totalPage - 1)) {
     blog.previousStatePage = blog.currentPage;
     blog.currentPage++;
     showPosts();
-    setIndicatoreAttivo(blog.currentPage, blog.previousStatePage);
-  } else if (scrollTop <= 1 && blog.currentPage > 0) {
+    setIndicatoreAttivo();
+  }
+  // Scroll verso l'alto - torna alla pagina precedente
+  else if (scrollTop <= 1 && blog.currentPage > 0) {
     blog.previousStatePage = blog.currentPage;
     blog.currentPage--;
     showPosts();
-    setIndicatoreAttivo(blog.currentPage, blog.previousStatePage);
+    setIndicatoreAttivo();
+
+    // Scroll automatico alla fine del contenuto della pagina precedente
+    setTimeout(() => {
+      // Calcola l'altezza che dovrebbe avere il contenuto fino alla pagina corrente
+      let targetHeight = (blog.currentPage + 1) * blog.itemsPerPage * 200; // stima altezza per post
+      window.scrollTo(0, document.documentElement.scrollHeight - window.innerHeight - 50);
+    }, 100);
   }
 });
 
@@ -50,7 +61,10 @@ async function initBlog() {
   blog.totalPage = Math.ceil(blog.posts.length / blog.itemsPerPage);
   initIndicatoriPaginazione();
   showPosts();
+  setIndicatoreAttivo(); // Imposta l'indicatore iniziale
 }
+
+initBlog();
 
 function initIndicatoriPaginazione() {
   for (let i = 0; i < blog.totalPage; i++) {
@@ -61,11 +75,26 @@ function initIndicatoriPaginazione() {
 }
 
 function showPosts() {
-  let start = blog.currentPage * blog.itemsPerPage;
-  for (let i = start; i < start + blog.itemsPerPage; i++) {
-    const post = blog.posts[i];
-    if (!post) break;
-    blog.blogWrapper.innerHTML += createPostHTML(post, i);
+  // Quando si va indietro, svuota il contenuto e ricarica tutti i post fino alla pagina corrente
+  if (blog.currentPage < blog.previousStatePage) {
+    blog.blogWrapper.innerHTML = '';
+    // Carica tutti i post dalla pagina 0 alla pagina corrente
+    for (let page = 0; page <= blog.currentPage; page++) {
+      let start = page * blog.itemsPerPage;
+      for (let i = start; i < start + blog.itemsPerPage; i++) {
+        const post = blog.posts[i];
+        if (!post) break;
+        blog.blogWrapper.innerHTML += createPostHTML(post, i);
+      }
+    }
+  } else {
+    // Quando si va avanti, aggiungi solo i nuovi post
+    let start = blog.currentPage * blog.itemsPerPage;
+    for (let i = start; i < start + blog.itemsPerPage; i++) {
+      const post = blog.posts[i];
+      if (!post) break;
+      blog.blogWrapper.innerHTML += createPostHTML(post, i);
+    }
   }
 }
 
@@ -78,5 +107,3 @@ function createPostHTML(post, index) {
     </article>
   `;
 }
-
-initBlog()
